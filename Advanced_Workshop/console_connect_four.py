b94458f394d9fc0075f6061bd265aa2f127bf618
Hristo Lakov <hristo.lakov33@gmail.com>
2022-08-08 08:29:11 +0300

    Excercising..

--- Advanced_Workshop/console_connect_four.py
+++ Advanced_Workshop/console_connect_four.py
@@ -30,10 +30,52 @@ def place_player_choice(ma, selected_col_index, player_num):
             return row_index, selected_col_index
     raise FullColumnError
 
-def is_winner(ma, row, col, player_num, slots_count = 4):
-    is_right = all[ma[row][col+index] == player_num for index in range(slots_count)]
-    is_left = all[ma[row][col-index] == player_num for index in range(slots_count)]
 
+def is_horizontal(ma, row, col, player_num, slots_count):
+    count_right = [is_player_num(ma, row, col + index, player_num) for index in range(slots_count)].count(True)
+    count_left = [is_player_num(ma, row, col - index, player_num) for index in range(slots_count)].count(True)
+    return (count_left + count_right) > slots_count
+
+
+def is_right_diagonal(ma, row, col, player_num, slots_count):
+    right_up_count = [is_player_num(ma, row - index, col + index, player_num) for index in range(slots_count)].count(True)
+    left_down_count = [is_player_num(ma, row + index, col - index, player_num) for index in range(slots_count)].count(True)
+    return (right_up_count + left_down_count) > slots_count
+
+
+def is_left_diagonal(ma, row, col, player_num, slots_count):
+    left_up_count = [is_player_num(ma, row - index, col - index, player_num) for index in range(slots_count)].count(True)
+    right_down_count = [is_player_num(ma, row + index, col + index, player_num) for index in range(slots_count)].count(True)
+    return (left_up_count + right_down_count) > slots_count
+
+
+def is_up(ma, row, col, player_num, slots_count):
+    up_count = [is_player_num(ma, row - index, col, player_num) for index in range(slots_count)].count(True)
+    return up_count > slots_count
+
+
+def is_winner(ma, row, col, player_num, slots_count = 4):
+    if any(
+            [
+                is_horizontal(ma, row, col, player_num, slots_count),
+                is_right_diagonal(ma, row, col, player_num, slots_count),
+                is_left_diagonal(ma, row, col, player_num, slots_count),
+                is_up(ma, row, col, player_num, slots_count)
+            ]
+    ):
+        return True
+    return False
+
+
+def is_player_num(ma, row, col, player_num):
+    if col < 0:
+        return False
+    try:
+        if ma[row][col] == player_num:
+            return True
+    except IndexError:
+        return False
+    return False
 
 
 rows = 6
@@ -53,9 +95,8 @@ while True:
         validate_col_choice(col_num, cols - 1)
         row, col = place_player_choice(matrix, col_num, player_num)
         if is_winner(matrix, row, col, player_num):
+            print_matrix(matrix)
             print(f"Player {player_num} has won the game!")
-            break
-        elif board_full():
             break
         print_matrix(matrix)
     except InvalidColumnError:
