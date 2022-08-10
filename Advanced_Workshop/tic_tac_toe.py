f9605b1fa0bae21d38f14746b90ea05547157960
Hristo Lakov <hristo.lakov33@gmail.com>
2022-08-10 21:12:55 +0300

    building tic tac toe along with the Lecturer

--- Advanced_Workshop/tic_tac_toe.py
+++ Advanced_Workshop/tic_tac_toe.py
@@ -1,1 +1,21 @@
-#print matrix
+class Player:
+    def __init__(self, name, sign):
+        self.name = name
+        self.sign = sign
+
+
+def read_players():
+    first_player_name = input(f"Player 1 name: ")
+    second_player_name = input(f"Player 2 name: ")
+
+    while True:
+        first_player_sign = input(f"{first_player_name} you would like to play with X or O?").upper()
+        if first_player_sign == "X" or first_player_sign == "O":
+            break
+
+    second_player_sign = "O" if first_player_sign == "X" else "X"
+
+
+    return first_player_name, second_player_name
+
+first_player, second_player = read_players()
